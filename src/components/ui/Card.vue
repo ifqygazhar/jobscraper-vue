63fae565f28ad2d401725a2434967b43e562ba8a
ifqygazhar <ifqygazhar@gmail.com>
2025-05-07 07:27:06 +0700

    feat:Enhance Card and IconAndText components with active state styling and interactivity

--- src/components/ui/Card.vue
+++ src/components/ui/Card.vue
@@ -1,19 +1,38 @@
 <template>
   <div
-    class="rounded-[0.8rem] p-4 bg-black w-[80vw] sm:w-[70vw] md:w-[60vw] lg:w-[58vw] xl:w-[58vw] overflow-auto"
+    :class="{
+      'bg-black ': isCardActive,
+      'border-2 bg-transparent ': !isCardActive,
+    }"
+    class="rounded-[0.8rem] p-4 w-[80vw] sm:w-[70vw] md:w-[60vw] lg:w-[58vw] xl:w-[58vw] overflow-auto cursor-pointer"
+    @click="toggleCardActive"
   >
     <div class="flex flex-col flex-wrap">
       <div class="flex flex-row justify-between mb-1.5">
-        <p class="text-white text-[min(4vw,1.2rem)]">Programmer</p>
+        <p :class="isCardActive ? 'text-white ' : 'text-black '">Programmer</p>
         <div class="flex flex-row flex-wrap items-center">
-          <div class="rounded bg-white mr-2">
+          <div
+            :class="{
+              'bg-white acitve:bg-black': isCardActive,
+              'bg-black ': !isCardActive,
+            }"
+            class="rounded mr-2"
+          >
             <img
               class="h-5 w-5 object-contain"
               src="https://images.glints.com/unsafe/glints-dashboard.oss-ap-southeast-1.aliyuncs.com/company-logo/43349064a3f558d1f4095294fc357137.jpeg"
               alt="company logos"
             />
           </div>
-          <p class="text-gray-200 text-[min(4vw,0.9rem)]">Company name</p>
+          <p
+            :class="
+              isCardActive
+                ? 'text-gray-200 '
+                : 'text-black active:text-gray-200'
+            "
+          >
+            Company name
+          </p>
         </div>
       </div>
       <div class="flex flex-col flex-wrap mb-2">
@@ -21,11 +40,16 @@
           class="mb-2"
           :icon="MapPinIcon"
           :title="'Sample Location'"
+          :is-card-active="isCardActive"
         />
-        <IconAndText :icon="BanknotesIcon" :title="'Sallary'" />
+        <IconAndText
+          :icon="BanknotesIcon"
+          :title="'Sallary'"
+          :is-card-active="isCardActive"
+        />
       </div>
     </div>
-    <button class="bg-jobstreet rounded w-full p-1.5">
+    <button class="bg-jobstreet rounded w-full p-1.5 cursor-pointer">
       <div class="flex flex-row justify-center items-center">
         <p class="text-white text-[min(4vw,0.8rem)] mr-2">Apply</p>
         <Jobstreet />
@@ -38,4 +62,11 @@
 import { BanknotesIcon, MapPinIcon } from "@heroicons/vue/20/solid";
 import IconAndText from "./IconAndText.vue";
 import Jobstreet from "../icon/Jobstreet.vue";
+import { ref } from "vue";
+
+const isCardActive = ref(false);
+
+const toggleCardActive = () => {
+  isCardActive.value = !isCardActive.value;
+};
 </script>
--- src/components/ui/IconAndText.vue
+++ src/components/ui/IconAndText.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="flex flex-row flex-wrap items-center">
-    <div class="bg-white rounded h-6 w-6 flex items-center justify-center">
-      <component :is="icon" class="size-5" />
+    <div
+      :class="{
+        'bg-white': isCardActive,
+        'bg-black': !isCardActive,
+      }"
+      class="rounded h-6 w-6 flex items-center justify-center"
+    >
+      <component
+        :is="icon"
+        :class="{
+          'text-white': !isCardActive,
+          'text-black': isCardActive,
+        }"
+        class="size-5"
+      />
     </div>
-    <p class="ml-2 text-gray-200 text-[min(4vw,0.9rem)]">{{ title }}</p>
+    <p
+      :class="{
+        'text-gray-200': isCardActive,
+        'text-black': !isCardActive,
+      }"
+      class="ml-2 text-[min(4vw,0.9rem)]"
+    >
+      {{ title }}
+    </p>
   </div>
 </template>
 
@@ -11,6 +32,11 @@
 import { MapPinIcon } from "@heroicons/vue/24/solid";
 
 const props = defineProps({
+  isCardActive: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
   title: {
     type: String,
     required: true,
