e70745a3e3b358dcc24c0260cffbb102633eac9c
ifqygazhar <ifqygazhar@gmail.com>
2025-06-22 19:45:12 +0700

    feat: Add API endpoints and sample responses for job listings

---
+++ src/api/endpoint.ts
@@ -0,0 +1,6 @@
+export const API_URL = "https://localhost:5000/api";
+
+export const glints = `${API_URL}/glints`;
+export const remoteok = `${API_URL}/remoteok`;
+export const jobstreet = `${API_URL}/jobstreet`;
+export const indeed = `${API_URL}/indeed`;
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,9 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
-import { createRouter, createWebHistory } from "vue-router";
 
+const app = createApp(App);
 const pinia = createPinia();
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      component: App,
-    },
-  ],
-});
 
-createApp(App).mount("#app");
+app.use(pinia);
+app.mount("#app");
